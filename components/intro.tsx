dbedb7671216a251579d06826dad6fb88c4f8f9b
hemilpatel971 <hemilpatel97@gmail.com>
2024-03-24 16:13:24 -0400

    test

--- components/intro.tsx
+++ components/intro.tsx
@@ -48,7 +48,7 @@ export default function Intro() {
       </div>
 
       <motion.h1
-        className="mb-10 mt-4 px-4 text-xl font-small !leading-[1.5] sm:text-lg"
+        className="mb-10 mt-4 px-4 text-xl !leading-[1.5] sm:text-lg"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
@@ -74,7 +74,7 @@ export default function Intro() {
       </motion.h1>
 
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
